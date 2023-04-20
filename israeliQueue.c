f096199725c489bc909ad9bb55ed3f8075d3410b
AdMatay <129521224+AdMatay@users.noreply.github.com>
2023-04-20 16:19:56 +0300

    Create israeliQueue.c

---
+++ israeliQueue.c
@@ -0,0 +1,24 @@
+#include <stdio.h>      
+#include <stdlib.h>     
+#include "tools/israeliqueue.h"
+
+//creating the nodes of people in our list with the relevant values
+typedef struct Person 
+{
+int friendsPassed;
+int enemyHeldBack;
+struct Person* next;
+} Person;
+
+struct IsraeliQueue_t
+{
+ person* head;
+ int friendshipCounter=0; //for realloc and free
+   FriendshipFunction* friendshipFunctions; 
+   ComparisonFunction compare;
+ int friendship_th;
+ int rivalry_th;
+
+}
+
+
